3b6ebb4c33a18ccba1071c0003ddd51ce9a6f3a8
Veronica Kim <vero.a.kim@gmail.com>
2018-02-27 01:40:06 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,12 @@
 class Dog
+  attr_accessor :mood
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+
   # code goes here
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,39 @@
 class Owner
-  # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  @@all = []
+
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    @species = "human"
+    @pets = {fishes: [], dogs: [], cats: []}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def say_species
+    "I am a human."
+  end
+
+  def self.count
+    self.all.count
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def buy_fish(name)
+    fish = Fish.new(name)
+    pets[:fishes] << fish
+   pets[:fishes].count
+  end
+
+
+
 end
