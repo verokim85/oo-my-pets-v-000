4398a13e047cb92805380b2973f5866d57bdba62
Veronica Kim <vero.a.kim@gmail.com>
2018-02-27 03:01:16 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -64,7 +64,7 @@ class Owner
     cat = self.pets[:cats].count
     fish = self.pets[:fishes].count
 
-p   return "I have #{fish} fish, #{dog} dog(s), and #{cat} cat(s)."
+p   puts "I have #{fish} fish, #{dog} dog(s), and #{cat} cat(s)."
   end
 
   # def sell_pets
